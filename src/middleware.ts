77402688a62633a279265287815ef8ea10aaa3a9
vstocks2024 <vstocks2024@gmail.com>
2024-10-28 05:26:05 +0530

    updated middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,6 +3,8 @@ import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
   // const token = request.cookies.has("token");
+  if(request.nextUrl.pathname==="/") return NextResponse.redirect(new URL("/dashboard/vectors",request.url));
+  if(request.nextUrl.pathname==="/dashboard/uploads") return NextResponse.redirect(new URL("/dashboard/uploads/videos",request.url));  
   // if (!token)
   //   return NextResponse.redirect(new URL("/admin/login", request.url));
 }
